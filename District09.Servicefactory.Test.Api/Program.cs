f95e01f096140b123dbb46dc6d01b67649cf2ad8
Jefwillems <jefwillems100@gmail.com>
2022-03-15 15:52:48 +0100

    fix: formatting

--- District09.Servicefactory.Test.Api/Program.cs
+++ District09.Servicefactory.Test.Api/Program.cs
@@ -18,15 +18,12 @@ builder.Services.AddAmqpMessaging(builder.Configuration,
     {
         opts
             .WithApmTracing<MyData>()
-            .WithListenerMiddleware<MyDataMiddleware,
-                MyData>() // For MyData processing -> first execute our middleware (for putting data in context or something)
-            .WithListener<MyData,
-                MyMessageHandler>("some.queue.mydata") // only after middleware is executed, handle the message
+            .WithListenerMiddleware<MyDataMiddleware, MyData>() // For MyData processing -> first execute our middleware (for putting data in context or something)
+            .WithListener<MyData, MyMessageHandler>("some.queue.mydata") // only after middleware is executed, handle the message
             .WithPublisher<MyData>("some.queue.mydata");
 
         opts.WithListener<MySecondData, MySecondHandler>("some.queue.seconddata")
-            .WithListenerMiddleware<MySecondMiddleware,
-                MySecondData>() // in this case, middleware is executed after the message handler
+            .WithListenerMiddleware<MySecondMiddleware, MySecondData>() // in this case, middleware is executed after the message handler
             .WithPublisher<MySecondData>("some.queue.seconddata");
 
         return opts.Build();
